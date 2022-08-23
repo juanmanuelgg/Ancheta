a0de4faf10cfdc30e70c03f76dd90cb2fb070840
Juan Manuel <jmgonzalezgarzon@gmail.com>
2022-08-23 03:43:11 -0500

    Estabilización minima del proyecto

--- Auxiliares/Ejemplo/runLinux.sh
+++
@@ -1,5 +0,0 @@
-clear
-pwd
-javac Calculator.java
-javac -cp .:junit-4.12.jar CalculatorTest.java
-java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore CalculatorTest
--- Matematicas/Test/runLinux.sh
+++
@@ -1,9 +0,0 @@
-clear
-pwd
-
-javac ../src/Complex.java
-mv ../src/Complex.class Complex.class
-javac -cp .:junit-4.12.jar ComplexTest.java
-java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore ComplexTest
-
-rm *.class
